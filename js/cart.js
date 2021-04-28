a88457ffdef72567537b2a7c4356a07fcc54dc21
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-04-29 02:39:01 +0300

    add the catelog part

--- js/cart.js
+++ js/cart.js
@@ -19,17 +19,19 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-
+function clearCart() {}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
+  // TODO: Find the table body
 
+  // TODO: Iterate over the items in the cart
   // TODO: Create a TR
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
-
+}
 
 function removeItemFromCart(event) {
 
@@ -37,4 +39,7 @@ function removeItemFromCart(event) {
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
 
+}
 
+// This will initialize the page and draw the cart on screen
+renderCart();
